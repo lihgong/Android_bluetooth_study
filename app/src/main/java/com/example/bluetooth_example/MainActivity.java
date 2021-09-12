28f9efe63407a1e7da34d33dae75d94c66150754
lihgong <lihgong.cm90@nctu.edu.tw>
2021-09-12 22:45:17 +0800

    Add a list to show the bonded devices

--- app/src/main/java/com/example/bluetooth_example/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_example/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.bluetooth_example;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter; // for Bluetooth
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
 
 public class MainActivity extends AppCompatActivity {
     private Button On,Off,list;
+    private ListView lv;
     private static final String TAG = "QQM";
     BluetoothAdapter BT = BluetoothAdapter.getDefaultAdapter();
+    private Set<BluetoothDevice>pairedDevices;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         On = (Button)findViewById(R.id.button_on);
         Off = (Button)findViewById(R.id.button_off);
         list = (Button)findViewById(R.id.button_list);
+        lv = (ListView)findViewById(R.id.bt_listview);
     }
 
     public void button_cb_on(View view) {
@@ -54,6 +62,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void button_cb_list(View view) {
         Log.i(TAG, "button_cb_list");
+        pairedDevices = BT.getBondedDevices();
 
+        ArrayList list = new ArrayList();
+        for(BluetoothDevice bt : pairedDevices) {
+            list.add(bt.getName());
+        }
+
+        Toast.makeText(getApplicationContext(),"Showing Paired Devices", Toast.LENGTH_SHORT).show();
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
     }
 }
