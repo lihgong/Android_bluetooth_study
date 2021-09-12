184695ad5b1a16b72963186a754b9aea6445236c
lihgong <lihgong.cm90@nctu.edu.tw>
2021-09-12 20:42:10 +0800

    1. Add log message 2. In Android studio, log message is in view/Tool/Event Log

--- app/src/main/java/com/example/bluetooth_example/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_example/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.bluetooth_example;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "QQM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "MyClass.getView() 1");
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "MyClass.getView() 2");
     }
 }
